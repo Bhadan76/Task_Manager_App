7527597a8b2adacde9d38644eb404baf3cd0e4fe
Bhadan <bhadanpaul@gmail.com>
2026-06-07 18:20:15 +0600

    Images/assets path section

---
+++ lib/ui/utils/AssetPaths.dart
@@ -0,0 +1,6 @@
+class Assetpaths {
+  static const String _imgPath = 'assets/images';
+  static const String BackgroundImg = '$_imgPath/app_Background.jpg';
+  static const String LogoImg = '$_imgPath/LogoSvg.svg';
+
+}
