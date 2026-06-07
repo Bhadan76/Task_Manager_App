d7671ad155dc80fcc8b5b1665809bad9b8183979
Bhadan <bhadanpaul@gmail.com>
2026-06-07 18:21:50 +0600

    Center progress indegator

---
+++ lib/ui/widgets/center_progress_indegator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Center_progress_indegator extends StatelessWidget {
+  const Center_progress_indegator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
